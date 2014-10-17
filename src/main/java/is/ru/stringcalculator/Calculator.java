06c221ba84806c09824df8bf403cd4fa870bfef8
Telma Gudbjorg Eythorsdottir <telma@i-040a14f7.greenqloud.internal>
2014-10-17 14:31:36 +0000

    Created project skelaton Created build script under bin, downloaded jar files under lib and created calculator test and files under src/ Method Add can take empty string. Test 1.b passed

---
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -0,0 +1,27 @@
+package is.ru.stringcalculator;
+
+public class Calculator{
+
+
+    public static int Add(String numbers){
+	
+	if(numbers.isEmpty()){return 0;}
+	
+	
+
+	return 10;
+	
+    
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,26 @@
+package is.ru.stringcalculator;
+
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest{
+
+    public static void main(String args[]){
+
+	org.junit.runner.JUnitCore.main("is.ru.StringCalculator.CalculatorTest");
+    }
+
+    @Test
+    public void testEmptyString(){
+
+	assertEquals(0, Calculator.Add(""));
+
+}
+
+
+
+
+
+
+}
